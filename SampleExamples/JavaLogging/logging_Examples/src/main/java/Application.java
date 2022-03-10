e7e25fec8a4a2766dbdb2235f81c46458b548222
MallikMandava <mmandava@flexera.com>
2022-03-10 15:21:08 +0530

    Adding File

--- SampleExamples/JavaLogging/logging_Examples/src/main/java/Application.java
+++ SampleExamples/JavaLogging/logging_Examples/src/main/java/Application.java
@@ -1,5 +1,8 @@
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Logger;
+//import org.apache.logging.log4j.
+import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.LoggerContext;
 
 /*
    I am application Developer wants to use math library
@@ -14,19 +17,21 @@ public class Application {
 
     public static void main(String[] args) {
         //Creating the Logger object
-        BasicConfigurator.configure();
-        final  Logger logger = Logger.getLogger(Application.class);
+       // BasicConfigurator.configure();
+        Configurator.initialize(null,"E:\\Testing_code\\logging_Examples\\src\\main\\resources\\log4j2.xml");
+
+        final  Logger logger =  LogManager.getLogger(Application.class);
        // Logger logger = LoggerFactory.getLogger("SampleLogger");
 
         //Logging the information
-        logger.info("Hi This is my first Logging  program using log4j1.2" );
-      //  MathLibrary ML = new MathLibrary();
-       // int result = ML.Add(10,15);
-       // logger.info("The addition of two values are " + result );
+        logger.info("Hi This is my first Logging  program using log4j 2.x" );
+        MathLibrary ML = new MathLibrary();
+       int result = ML.Add(10,15);
+       logger.info("The addition of two values are " + result );
 
 
-      ////  UILibrary ui = new UILibrary();
-      //  ui.ThreeDView(12,45,67);
+        UILibrary ui = new UILibrary();
+       ui.ThreeDView(12,45,67);
     }
 }
 
